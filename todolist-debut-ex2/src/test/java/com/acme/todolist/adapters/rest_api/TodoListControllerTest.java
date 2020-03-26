3226e3e39b4c46d56bafece0cdb9f7069bb7e9ce
Alexis Petit <alexispetit1@protonmail.com>
2020-03-26 21:18:02 +0100

    unused imports

--- todolist-debut-ex2/src/test/java/com/acme/todolist/adapters/rest_api/TodoListControllerTest.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/adapters/rest_api/TodoListControllerTest.java
@@ -1,22 +1,15 @@
 package com.acme.todolist.adapters.rest_api;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.Instant;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.acme.todolist.domain.TodoItem;
