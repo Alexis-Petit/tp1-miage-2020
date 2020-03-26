e9217b67f1280c367a4bd5f5b0181689aadff6a8
DoFaure <dorianfaure69@gmail.com>
2020-03-26 22:12:14 +0100

    modification test integration

--- todolist-debut-ex2/src/test/java/com/acme/todolist/adapters/rest_api/TodoListControllerTest.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/adapters/rest_api/TodoListControllerTest.java
@@ -1,33 +1,42 @@
 package com.acme.todolist.adapters.rest_api;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.Instant;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.acme.todolist.application.port.in.AddTodoItems;
+import com.acme.todolist.application.port.in.GetTodoItems;
 import com.acme.todolist.domain.TodoItem;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@WebMvcTest
-@ActiveProfiles
+@ContextConfiguration(classes = TodoListController.class)
+@WebMvcTest(controllers = TodoListController.class)
+@AutoConfigureMockMvc
 public class TodoListControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @MockBean
+    private AddTodoItems addTodoItemsQuery;
+    
+    @MockBean
+    private GetTodoItems getTodoItemsQuery;
+    
 
     @Test
     public void shouldPostLateMessage() throws Exception {
